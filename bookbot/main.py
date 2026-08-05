5aaa393812b42cd88c5b5ed7c9bb00d0df346039
Michael Villarreal <mikev4359@gmail.com>
2026-08-04 22:35:50 -0500

    Included tuple to list function as well as the sorting of the list. Next is organizing for report format.

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,4 +1,4 @@
-from stats import count_words, create_dict
+from stats import count_words, create_dict, chars_dict_to_sorted_list
 
 def get_book_text(path_to_file: str):
     with open(path_to_file) as f:
@@ -8,5 +8,6 @@ def get_book_text(path_to_file: str):
 book_contents = get_book_text("books/frankenstein.txt")
 count = count_words(book_contents)
 letter_counts = create_dict(book_contents)
+sorted_dict = chars_dict_to_sorted_list(letter_counts)
 print(f"Found {count} total words")
-print(dict)
+print(sorted_dict)
--- bookbot/stats.py
+++ bookbot/stats.py
@@ -1,3 +1,8 @@
+def get_book_text(path_to_file: str):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
 def count_words(book: str):
 
     words = len(book.split())
@@ -15,3 +20,21 @@ def create_dict(book: str):
             full_dict[letter] = 1
 
     return full_dict
+
+def sort_on(a_tuple: tuple[str, int]):
+    value_count = 0
+
+
+    return a_tuple[1]
+
+def chars_dict_to_sorted_list(some_dictionary):
+    converted_list = []
+    sorted_converted_list = []
+    adding_tuple = ()
+
+    for key in some_dictionary:
+        adding_tuple = (key, some_dictionary[key])
+        converted_list.append(adding_tuple)
+
+    sorted_converted_list = sorted(converted_list, reverse=True, key=sort_on)
+    return sorted_converted_list
